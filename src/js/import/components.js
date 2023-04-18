fcc48c686295d6ab74f10d9d0814cf3f1d88688d
Иван <i.tarchevsky@gmail.com>
2023-04-18 16:15:55 +0300

    init: project

---
+++ src/js/import/components.js
@@ -0,0 +1,19 @@
+//! Basic
+
+import "%components%/burger/burger";
+
+//! Other
+
+// import "%components%/tabs/tabs";
+// import "%components%/dropdown/dropdown";
+import "%components%/modal/modal";
+// import "%components%/gallery/gallery";
+// import "%components%/flex-gallery/flex-gallery";
+import "%components%/slider/slider";
+import "%components%/accordion/accordion";
+// import "%components%/show-more/show-more";
+// import "%components%/tabs-tags/tabs-tags";
+// import "%components%/dropdown-lang/dropdown-lang";
+// import "%components%/breadcrumbs/breadcrumbs";
+// import "%components%/circle/circle";
+// import "%components%/quiz/quiz";
